e62d6f5ebe84dbe026d2fe5c6959f0a3a5a2a16d
RYESAN58 <92766524+RYESAN58@users.noreply.github.com>
2022-02-10 17:07:00 -0500

    finished assigment

--- flask_app/controllers/ninjas.py
+++ flask_app/controllers/ninjas.py
@@ -8,7 +8,7 @@ def index():
     return render_template("index.html")
 @app.route('/dojos')
 def schools():
-    friends= Dojo.get_all()
+    friends = Dojo.get_all()
     return render_template('dojos.html', all_dojos = friends)
 @app.route('/create_dojo', methods = ['POST'])
 def create():
@@ -18,3 +18,23 @@ def create():
     print(data)
     friends = Dojo.save(data)
     return redirect('/dojos')
+@app.route('/dojos/<int:num>/<x>')
+def show(num,x):
+    friends = Ninja.get_dojos(num)
+    session['x'] = x
+    return render_template('show.html', num = num, all_nijas = friends, x = session['x'])
+
+@app.route('/ninja')
+def ninja():
+    friends = Ninja.get_all()
+    places = Dojo.get_all()
+    return render_template("ninjas.html",all_ninjas = friends, all_dojos = places)
+@app.route('/add_ninja', methods=['POST'])
+def set_ninja():
+    data = {
+        'dojo_id':request.form['dojo_id'],
+        'fname':request.form['fname'],
+        'lname':request.form['lname'],
+    }
+    friends = Ninja.add_to_dojo(data)
+    return redirect('/')
--- flask_app/models/ninja.py
+++ flask_app/models/ninja.py
@@ -3,8 +3,8 @@ from flask_app.config.mysqlconnection import connectToMySQL
 class Ninja:
     def __init__( self , data ):
         self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
+        self.first_name = data['firstname']
+        self.last_name = data['lastname']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     @classmethod
@@ -17,7 +17,7 @@ class Ninja:
         return x
     @classmethod
     def save(cls, data ):
-        query = "INSERT INTO ninjas ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
+        query = "INSERT INTO ninjas ( firstname , lastname , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
         return connectToMySQL('dojos_ninjas').query_db( query, data)
     @classmethod
     def remove(cls, num ):
@@ -27,3 +27,11 @@ class Ninja:
     def update(cls, data):
         query= "UPDATE `ninjas`.`dojos_ninjas` SET `first_name` = %(fname)s, `last_name` = %(lname)s, `email` = %(email)s WHERE (`id` =  %(id)s);"
         return connectToMySQL('dojos_ninjas').query_db( query,data)
+    @classmethod
+    def get_dojos(cls, num1):
+        query = f"SELECT ninjas.firstname ,ninjas.lastname ,ninjas.dojo_id ,dojos.id, dojos.name FROM ninjas JOIN dojos ON ninjas.dojo_id = dojos.id WHERE dojos.id = {num1}"
+        return connectToMySQL('dojos_ninjas').query_db(query)
+    @classmethod
+    def add_to_dojo(cls, data):
+        query = "INSERT INTO `dojos_ninjas`.`ninjas` (`firstname`, `lastname`, `dojo_id`) VALUES (%(fname)s, %(lname)s,%(dojo_id)s);"
+        return connectToMySQL('dojos_ninjas').query_db(query, data)
